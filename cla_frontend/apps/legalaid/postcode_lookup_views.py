31fd5bbc816089f0fb502a7fdc707c8792036d59
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-12-14 09:37:21 +0000

    Work in progress - tests return unexpected status code

--- cla_frontend/apps/legalaid/postcode_lookup_views.py
+++ cla_frontend/apps/legalaid/postcode_lookup_views.py
@@ -1,74 +1,18 @@
 # -*- encoding: utf-8 -*-
 """View for postcode lookups"""
 
-# from django.contrib.auth.decorators import login_required
-
-# import postcodeinfo
 from django.conf import settings
 from cla_common.address_lookup.ordnance_survey import FormattedAddressLookup
+from django.http import JsonResponse
 
-
-# @login_required
 def postcode_lookup(request):
-    print("REQUEST", request)
     postcode = request.GET.get('postcode')
-    print(postcode)
     key = settings.OS_PLACES_API_KEY
     formatted_address = FormattedAddressLookup(key=key).by_postcode(postcode)
-    print("formatted_address", formatted_address)
+    # print("FORMATED ADDRESS", formatted_address)
     response = [{'formatted_address': address } for address in formatted_address if address]
-    return Response(response)
-
-
-#     try:
-#         return addresses_response(request)
-
-#     except postcodeinfo.NoResults:
-#         return no_results_response()
-
-#     except postcodeinfo.PostcodeInfoException as e:
-#         return error_response(e)
-
-
-# def addresses_response(request):
-#     return JsonResponse(
-#         addresses_for_postcode_from_url_params(request),
-#         safe=False
-#     )
-
-
-# def addresses_for_postcode_from_url_params(request):
-#     postcode = request.GET.get('postcode')
-#     addresses = postcodeinfo.Client().lookup_postcode(postcode).addresses
-#     print("ADDRESSESS inside addresses_for_postcode_from_url_params", addresses)
-#     re
-    # return map(formatted_address_only, addresses)
-    # return postcode_addresses(postcode_from_url_params(request))
-
-
-# def postcode_from_url_params(request):
-#     return request.GET.get('postcode')
-
-
-# def postcode_addresses(postcode):
-#     addresses = postcodeinfo.Client().lookup_postcode(postcode).addresses
-#     return map(formatted_address_only, addresses)
-
-
-# def formatted_address_only(address):
-#     return dict(filter(formatted_address_item, address.iteritems()))
-
-
-# def formatted_address_item(item):
-#     key, val = item
-#     return key == 'formatted_address'
-
-
-# def no_results_response():
-#     print("Calling no_results_response")
-#     return JsonResponse([], safe=False, status=404)
-
-
-# def error_response(exc):
-#     print("Calling error_response")
-#     return JsonResponse({'error': str(exc)}, status=500)
+    # print("response", response)
+    response = JsonResponse(response, safe=False)
+    # print("response 2", response.content)
+    print("Response status_code", response.status_code)
+    return response
--- cla_frontend/apps/legalaid/tests/test_postcode_lookup_views.py
+++ cla_frontend/apps/legalaid/tests/test_postcode_lookup_views.py
@@ -6,101 +6,84 @@ import json
 import unittest
 
 import mock
-import postcodeinfo
-from legalaid.postcode_lookup_views import postcode_lookup
 
+from legalaid.postcode_lookup_views import postcode_lookup
+from cla_common.address_lookup.ordnance_survey import FormattedAddressLookup
 
 postcode = 'sw1a1aa'
-valid_addresses = [
-    {
-        "formatted_address": "Buckingham Palace\nLondon\nSW1A 1AA",
-    }
-]
+postcode_is_NONE = None
+malformed_postcode = 'skd99kdf12ws'
+postcode_is_valid_but_doesnt_exist = 'ms46kb'
+postcode_is_an_empty_string = " "
+# formatted_address = [
+#     {
+#         "formatted_address": "Buckingham Palace\nLondon\nSW1A 1AA",
+#     }
+# ]
+# formatted_address = [u'Buckingham Palace\nLondon\nSW1A 1AA']
+# formatted_address = [{"Buckingham Palace\nLondon\nSW1A 1AA"}]
+# # formatted_address = [{u'Buckingham Palace\nLondon\nSW1A 1AA'}]
 
+# @contextmanager
+# def patch_postcodeinfo(postcode, result):
+#     with mock.patch('cla_common.address_lookup.ordnance_survey.FormattedAddressLookup.by_postcode') as mock_method:
+#         mock_method.return_value = {"Buckingham Palace\nLondon\nSW1A 1AA"}
+#         lookup = mock_method.by_postcode
 
-@contextmanager
-def patch_postcodeinfo(postcode, result):
-    with mock.patch('postcodeinfo.Client') as Client:
-        client = Client.return_value
-        lookup = client.lookup_postcode
+#         if callable(result):
+#             lookup.side_effect = result
+#         else:
+#             lookup.return_value.addresses = result
 
-        if callable(result):
-            lookup.side_effect = result
-        else:
-            lookup.return_value.addresses = result
+#         yield
 
-        yield
-
-        client.assertCalled()
-        lookup.assertCalledWith(postcode)
+        # client.assertCalled()
+        # lookup.assertCalledWith(postcode)
 
 
 class PostcodeLookupViewsTest(unittest.TestCase):
 
     def setUp(self):
         self.request = mock.Mock()
-        self.request.GET.get.return_value = postcode
         self.request.user.is_authenticated.return_value = True
 
-    def assert_no_results(self, response):
-        self.assertEqual(404, response.status_code)
-        self.assertEqual('[]', response.content)
-
-    def assert_error(self, response):
-        print("RESPONSE error", response.status_code)
-        self.assertEqual(500, response.status_code)
-
     def assert_address_response(self, addresses, response):
-        self.assertEqual(200, response.status_code)
+        print("JSON DUMPS ADDRESS", json.dumps(addresses))
+        print("RESPONSE CONTENT", response.content)
         self.assertEqual(json.dumps(addresses), response.content)
 
-    # def test_no_results_response(self):
-    #     print("No result response", no_results_response())
-    #     self.assert_no_results(no_results_response())
+    def test_postcode_lookup_returns_an_address_if_postcode_is_valid(self):
+        self.request.GET.get.return_value = postcode
+        # with patch_postcodeinfo(postcode, formatted_address):
+        self.assertEqual(200, response.status_code)
+        response = postcode_lookup(self.request)
+        self.assert_address_response(formatted_address, response)
 
-    # def test_postcode_from_url_params(self):
-    #     with patch_postcodeinfo(postcode, valid_addresses):
-    #         addresses = addresses_for_postcode_from_url_params(self.request)
-    #         addresses = addresses[0]['formatted_address']
-    #         reobj = re.compile(r'(\b[A-Z]{1,2}[0-9][A-Z0-9]? [0-9][ABD-HJLNP-UW-Z]{2}\b)')
-    #         matchobj = reobj.search(addresses)
-    #         postcode_from_address = matchobj.group(1)
-    #         print("POSTCODE FROM ADDRESS", postcode_from_address)
-    #         print("POSTCODE", postcode)
-    #         # print("ADDRESS FOR POSTCODE FROM URL PARAMS",addresses[0]['formatted_address'])
-    #     self.assertEqual(postcode, postcode_from_address)
+    def test_postcode_lookup_returns_a_404_error_if_postcode_is_NONE(self):
+        self.request.GET.get.return_value = postcode_is_NONE
+        response = postcode_lookup(self.request)
+        print(response.status_code)
+        self.assertEqual(404, response.status_code)
+        self.assert_address_response([], response)
 
-    # def test_postcode_addresses(self):
-    #     with patch_postcodeinfo(postcode, valid_addresses):
-    #         addresses = postcode_addresses(postcode)
-    #         print("postcode addresses", addresses)
-    #         self.assertEqual(valid_addresses, addresses)
+    def test_postcode_lookup_returns_a_404_error_if__postcode_is_malformed(self):
+        print(self.request.GET.get.return_value)
+        self.request.GET.get.return_value = malformed_postcode
+        response = postcode_lookup(self.request)
+        self.assertEqual(404, response.status_code)
+        self.assert_address_response([], response)
 
-    # def test_addresses_for_postcode_from_url_params(self):
-    #     with patch_postcodeinfo(postcode, valid_addresses):
-    #         addresses = addresses_for_postcode_from_url_params(self.request)
-    #         print("ADDRESS", addresses)
-    #         self.assertEqual(valid_addresses, addresses)
 
-    def test_postcode_lookup(self):
-        with patch_postcodeinfo(postcode, valid_addresses):
-            response = postcode_lookup(self.request)
-            self.assert_address_response(valid_addresses, response)
+    def test_postcode_lookup_returns_an_empty_list_if_postcode_is_valid_but_doesnt_exist(self):
+        self.request.GET.get.return_value = postcode_is_valid_but_doesnt_exist
+        response = postcode_lookup(self.request)
+        self.assertEqual(200, response.status_code)
+        self.assert_address_response([], response)
 
-    def test_postcode_lookup_no_results(self):
+    def test_postcode_lookup_returns_a_404_error_list_if__postcode_is_an_empty_string(self):
+        self.request.GET.get.return_value = postcode_is_an_empty_string
+        response = postcode_lookup(self.request)
+        self.assertEqual(404, response.status_code)
+        self.assert_address_response([], response)
 
-        def raise_exception(*args):
-            raise postcodeinfo.NoResults()
 
-        with patch_postcodeinfo(postcode, raise_exception):
-            response = postcode_lookup(self.request)
-            self.assert_no_results(response)
-
-    def test_server_error(self):
-
-        def raise_exception(*args):
-            raise postcodeinfo.ServiceUnavailable()
-
-        with patch_postcodeinfo(postcode, raise_exception):
-            response = postcode_lookup(self.request)
-            self.assert_error(response)
